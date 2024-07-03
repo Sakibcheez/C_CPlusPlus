8f0b332003a430b7b886a2449e658a3c2274234e
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-07-04 03:54:03 +0600

    up

--- CPlusPlus/first.cpp
+++ CPlusPlus/first.cpp
@@ -5,14 +5,24 @@ using namespace std;
 int main()
 {
 cout << "Hello World!\n";
-cout << "Have a good day!";
+cout << "Have a good day!\n";
+ cout << "Hello Sakib!" << "\n";
+printf("\nHi bro\n");
+ cout << "Hello world!" << endl;
+ cout << "\n";
+ int myNum = 15;
+cout << myNum;
+ cout << "\nMy roll is " << myNum << endl;
+ int myNum1 = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false)
+cout << myFloatNum << "\n";
+cout << myLetter <<"\n";
+int x = 5;
+int y = 6;
+int sum = x + y;
+cout << sum;
 return 0;
 }
-
-/*
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
-*/
