96865b0685603be9fec76449dfa30b95c098017d
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-07-04 03:40:45 +0600

    save1

---
+++ CPlusPlus/first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "Hello World!\n";
+cout << "Have a good day!";
+return 0;
+}
+
+/*
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+*/
---
+++ CPlusPlus/include/first.h
@@ -0,0 +1,16 @@
+#ifndef FIRST_H
+#define FIRST_H
+
+
+class first
+{
+    public:
+        first();
+        virtual ~first();
+
+    protected:
+
+    private:
+};
+
+#endif // FIRST_H
