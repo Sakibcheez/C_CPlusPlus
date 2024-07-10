3a6bbb8a7c5b9f6b5be0688d46d0cad08cb02c88
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-07-11 02:37:50 +0600

    string last

--- CPlusPlus/fourth.cpp
+++ CPlusPlus/fourth.cpp
@@ -28,7 +28,44 @@ cout << endl ;
     cout << myString2;
 // Outputs Jello instead of Hello
 
+cout << endl ;
 
+// at() dunction
+
+string myString4 = "Hello";
+cout << myString4 << endl; // Outputs Hello
+
+cout << myString4.at(0);  // First character
+cout << myString4.at(1);  // Second character
+cout << myString4.at(myString.length() - 1) << endl;  // Last character
+
+
+myString4.at(0) = 'J';
+cout << myString4;  // Outputs Jello
+
+cout << endl;
+
+string firstName5;
+cout << "Type your first name: ";
+cin >> firstName5; // get user input from the keyboard // space pele ney nah
+cout << "Your name is: " << firstName5;
+
+// Type your first name: John ahmed
+// Your name is: John
+
+cout << endl ;
+
+
+ // Full string with white space
+    string fullName6;
+    cin.ignore(); // This clears the newline character left in the input buffer
+    cout << "Type your full name: ";
+    getline(cin, fullName6);
+    cout << endl;
+    cout << "Your name is: " << fullName6;
+
+// Type your full name: John Doe
+// Your name is: John Doe
 
 
 }
