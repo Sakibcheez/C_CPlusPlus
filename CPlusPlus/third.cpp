01093db84cc34b1a9c4dcc1a26b592fc63a7c3f9
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-07-11 02:12:22 +0600

    string

--- CPlusPlus/third.cpp
+++ CPlusPlus/third.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    // The + operator can be used between strings to add them together to make a new string.
+    //This is called concatenation:
+    string firstName = "John ";
+    string lastName = "Doe";
+    string fullName = firstName + lastName;
+    cout << fullName;
+
+    return 0;
+}
