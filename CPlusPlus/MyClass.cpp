1195b7f377a16735b916db2a6ac4b34129dc3da7
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-07-29 22:49:32 +0600

    up

---
+++ CPlusPlus/MyClass.cpp
@@ -0,0 +1,19 @@
+#include "MyClass.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15;
+  myObj.myString = "Some text";
+
+  // Print attribute values
+  cout << myObj.myNum << "\n";
+  cout << myObj.myString;
+  return 0;
+}
---
+++ CPlusPlus/MyClass.h
@@ -0,0 +1,12 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+
+class MyClass
+{
+     public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;
+};
+
+#endif // MYCLASS_H
