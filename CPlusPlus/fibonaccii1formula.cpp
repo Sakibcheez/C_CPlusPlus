88d6c5b3507a119dda51d6b93825dcc8d689c593
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-08-09 02:00:28 +0600

    fibonacci

---
+++ CPlusPlus/fibonaccii1formula.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+
+
+
+int F(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return F(n - 1) + F(n - 2);
+    }
+}
+
+int main() {
+    printf("%d\n", F(19));
+    return 0;
+}
+
+
