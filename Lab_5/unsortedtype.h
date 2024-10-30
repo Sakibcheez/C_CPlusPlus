ccb9a4d24456aa1c064ef649ecf3a9256bc192bc
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-10-30 23:39:09 +0600

    comment

--- Lab_5/unsortedtype.h
+++ Lab_5/unsortedtype.h
@@ -12,6 +12,12 @@ public :
     void InsertItem(ItemType);
     void DeleteItem(ItemType);
     void RetrieveItem(ItemType&, bool&);
+
+    // The & after ItemType indicates that this parameter is passed by reference,
+    //meaning the function operates directly on the original item rather than a copy.
+    //By passing ItemType by reference, any changes made to this parameter within the function will affect
+    //the original variable outside the function
+
     void ResetList();
     void GetNextItem(ItemType&);
 private:
